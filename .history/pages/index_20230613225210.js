4732276aae01325ea5c8d5adfe9c9ea488009ac4
Krish Sinha <krishsinha0304@gmail.com>
2023-06-14 14:26:53 +0530

    init

--- .history/pages/index_20230613225210.js
+++ .history/pages/index_20230613225210.js
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className="page-container">
 			<Toolbar />
-			<div className={styles.main}>
+			<div className="{styles.main}">
 				<h1>Next.js News App</h1>
 				<h3>Your one stop shop for the latest news articles</h3>
 			</div>
